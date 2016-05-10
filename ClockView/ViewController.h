d0059f6c57856d7eaab20001740a16886aaa5099
Mr.Q <qianlishun@mrq-macbook-pro.local>
2016-05-10 10:54:50 +0800

    Initial Commit

---
+++ ClockView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClockView
+//
+//  Created by Mr.Q on 16/5/10.
+//  Copyright © 2016年 QLS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
